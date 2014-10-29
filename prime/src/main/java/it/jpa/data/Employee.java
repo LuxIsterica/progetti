222f119ec8ee5c07db6ce088909509f0fb598607
cognolato <cognolatostudio@libero.it>
2014-10-29 15:19:20 +0100

    aggiornamento 28-10

---
+++ prime/src/main/java/it/jpa/data/Employee.java
@@ -0,0 +1,58 @@
+package it.jpa.data;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+@Entity
+@Table(name="EMP")
+public class Employee {
+	@Id  @GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="EMP_ID")
+	private int employeeId;
+	@Column(name="EMP_NAME")
+	private String employeeName;
+	@Column(name="EMP_HIRE_DATE")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date employeeHireDate;
+	@Column(name="EMP_SALARY")
+	private double employeeSalary;
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+	public Date getEmployeeHireDate() {
+		return employeeHireDate;
+	}
+
+	public void setEmployeeHireDate(Date employeeHireDate) {
+		this.employeeHireDate = employeeHireDate;
+	}
+
+	public double getEmployeeSalary() {
+		return employeeSalary;
+	}
+
+	public void setEmployeeSalary(double employeeSalary) {
+		this.employeeSalary = employeeSalary;
+	}
+}
