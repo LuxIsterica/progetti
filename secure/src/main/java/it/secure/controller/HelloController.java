013f122bd7f1540573def6c7439989af6451707a
cognolato <cognolatostudio@libero.it>
2014-10-29 16:17:33 +0100

    Agg al 29-10

---
+++ secure/src/main/java/it/secure/controller/HelloController.java
@@ -0,0 +1,34 @@
+package it.secure.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
+	public ModelAndView welcomePage() {
+
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security prove tecniche.");
+		model.addObject("message", "Questa è una pagina di benvenuto!");
+		model.setViewName("hello");
+		return model;
+
+	}
+
+	@RequestMapping(value = "/admin**", method = RequestMethod.GET)
+	public ModelAndView adminPage() {
+
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security prove tecniche sicurezza.");
+		model.addObject("message", "Questa è una pagina protetta!");
+		model.setViewName("admin");
+
+		return model;
+
+	}
+
+}
